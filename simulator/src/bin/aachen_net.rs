6a28fdef09d9701cbbeb6898f8db05965ca58095
Enrico Lovisotto <enricolovisotto@gmail.com>
2019-01-25 17:09:16 +0100

    Added queues for timings, better use of log crate

--- simulator/src/bin/aachen_net.rs
+++ simulator/src/bin/aachen_net.rs
@@ -17,9 +17,9 @@ fn main() {
 
     let mut client = TcpClientBuilder::default()
         .node_id(1)
-        .next_hop_id(2)
+        .next_hop_id(3)
         .dst_id(2)
-        .window_size(0 as usize)
+        .window_size(10 as usize)
         .t_repeat(2.0)
         .t_unusable(20.0)
         .build()
@@ -27,11 +27,27 @@ fn main() {
 
     let mut server = TcpServerBuilder::default()
         .node_id(2)
-        .next_hop_id(1)
+        .next_hop_id(4)
         .dst_id(1)
         .total_n_packets(3 as usize)
         .mtu_size(1000 as u64)
         .t0(1.0)
+        .build()
+        .unwrap();
+
+    let mut client_to_server = BlockingQueueBuilder::default()
+        .node_id(3)
+        .dest_id(2)
+        .max_queue(100 as usize)
+        .conn_speed(1000.0)
+        .build()
+        .unwrap();
+
+    let mut server_to_client = BlockingQueueBuilder::default()
+        .node_id(4)
+        .dest_id(1)
+        .max_queue(100 as usize)
+        .conn_speed(1000.0)
         .build()
         .unwrap();
 
@@ -44,6 +60,8 @@ fn main() {
     let mut nodes: HashMap<NodeId, &mut Node> = HashMap::new();
     nodes.insert(client.get_id(), &mut client);
     nodes.insert(server.get_id(), &mut server);
+    nodes.insert(client_to_server.get_id(), &mut client_to_server);
+    nodes.insert(server_to_client.get_id(), &mut server_to_client);
 
     let mut event_queue: BinaryHeap<Event> = BinaryHeap::new();
     event_queue.push(fire_event);
@@ -54,16 +72,16 @@ fn main() {
     while let Some(event) = event_queue.pop() {
         n_events += 1;
 
-        debug!("\nCurrent event: {:?}", event);
+        info!(" ");
+        info!("{:?}", event);
 
         let Event { time, msg, recipient } = event;
 
         let destination = nodes.get_mut(&recipient).unwrap();
         let new_events = destination.process_message(msg, time.into());
 
-        debug!("New events:");
         for event in &new_events {
-            debug!("- {:?}", event);
+            info!("-> {:?}", event);
         }
 
         event_queue.extend(new_events);
