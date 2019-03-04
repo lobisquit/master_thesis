e53b56cabd095c097f967de0e01d4bc03e29fd56
Enrico Lovisotto <enrico.lovisotto@tutanota.com>
2019-03-04 18:19:54 +0100

    Echanched graph: performance improvements needed

--- simulator/src/little_graph.rs
+++ simulator/src/little_graph.rs
@@ -23,7 +23,7 @@ impl Into<NodeId> for usize {
 
 #[derive(Default, Debug)]
 pub struct Graph {
-    pub nodes_fathers: HashMap<NodeId, NodeId>,
+    nodes_fathers: HashMap<NodeId, NodeId>,
     pub nodes_weight: HashMap<NodeId, u64>
 }
 
@@ -41,7 +41,7 @@ impl Graph {
         self.nodes_fathers.get(&node_id.into())
     }
 
-    pub fn get_weight<N>(&mut self, node_id: N) -> Option<&u64>
+    pub fn get_weight<N>(&self, node_id: N) -> Option<&u64>
     where N: Into<NodeId> {
         self.nodes_weight.get(&node_id.into())
     }
@@ -55,5 +55,49 @@ impl Graph {
             .filter(|candidate| !non_leaves.contains(candidate))
             .map(|x| *x)
             .collect::<Vec<NodeId>>()
+    }
+
+    pub fn get_children<N>(&self, node_id: N) -> Vec<NodeId>
+    where N: Into<NodeId> {
+        let node_id: NodeId = node_id.into();
+
+        self.nodes_fathers.iter()
+            .filter(|(_, v)| **v == node_id)
+            .map(|(k, _)| *k)
+            .collect::<Vec<NodeId>>()
+    }
+
+    fn is_ancestor(&self, wanted_node: NodeId, leaf: NodeId) -> bool {
+        let mut current_node = leaf;
+        while let Some(node) = self.get_father(current_node) {
+            if *node == wanted_node {
+                return true;
+            }
+            else {
+                current_node = *node;
+            }
+        }
+        false
+    }
+
+    pub fn get_routes<N>(&self, node_id: N) -> HashMap<NodeId, NodeId>
+    where N: Into<NodeId> {
+        let children = self.get_children(node_id.into());
+        let leaves = self.get_leaves();
+
+        let mut routes = HashMap::new();
+
+        for child in children {
+            let child_leaves = leaves
+                .iter()
+                .filter(|leaf| self.is_ancestor(child, **leaf))
+                .map(|x| *x)
+                .collect::<Vec<NodeId>>();
+
+            for leaf in child_leaves {
+                routes.insert(leaf, child);
+            }
+        }
+        routes
     }
 }
