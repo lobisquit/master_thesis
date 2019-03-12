6b3776d6b8147ae4fac0079d3d9a190e9588c6f8
Enrico Lovisotto <enrico.lovisotto@tutanota.com>
2019-03-12 10:03:46 +0100

    Better elisp script

--- simulator/src/bin/mover.el
+++ simulator/src/bin/mover.el
@@ -1,14 +1,19 @@
 (defun copy-project ()
+  (defvar remote-dir "/ssh:lovisott@login.dei.unipd.it:/home/lovisott/aachen_net/")
+  (mkdir remote-dir t)
+
+  (mkdir (concat remote-dir "results/") t)
+
   (copy-file
    "/home/enrico/Archivi/master_thesis/data/aachen_net/topology.txt"
-   "/ssh:lovisott@login.dei.unipd.it:/home/lovisott/topology.txt" t)
+   (concat remote-dir "topology.txt") t)
 
   (copy-file
    "/home/enrico/Archivi/master_thesis/simulator/target/release/aachen_net"
-   "/ssh:lovisott@login.dei.unipd.it:/home/lovisott/aachen_net" t)
+   (concat remote-dir "aachen_net") t)
 
   (copy-file
    "/home/enrico/Archivi/master_thesis/simulator/src/bin/aachen_net.job"
-   "/ssh:lovisott@login.dei.unipd.it:/home/lovisott/aachen_net.job" t))
+   (concat remote-dir "aachen_net.job") t))
 
 (copy-project)
