66d1e0ad6aadca08b42d206b908b4243c1e760db
Enrico Lovisotto <enricolovisotto@gmail.com>
2019-03-15 17:52:20 +0100

    Added bit of log, faster variable creation for leaves and mainframe

--- optimizer.py
+++ optimizer.py
@@ -26,9 +26,18 @@ def name(source, target=None, var='x'):
     else:
         return "{}_{}".format(var, source)
 
+print("Start")
+
+obj_func = 0
+mainframe_bw = 0
+
 # setup bandwith and its requirements from users
+n_leaves = 0
 for v in g.vertices():
     if g.vertex(v).in_degree() == 0:
+        n_leaves += 1
+        print("Leaf {}".format(v), end='\r')
+
         bw[v] = m.continuous_var(name=name(v, var='bw'))
         bwr[v] = 1 # TODO set proper value
 
@@ -38,32 +47,42 @@ for v in g.vertices():
         m.add_constraint(u[v] <= bw[v] / bwr[v],
                          ctname="utility def {}".format(v))
 
+        obj_func += u[v]
+        mainframe_bw += bw[v]
+
+print('n_leaves = {}'.format(n_leaves))
+
 # objective function
-obj_func = sum(u.values())
 m.set_objective('max', obj_func)
 
 # flow conditions
 mainframe = np.where(g.vp['root'].a)[0][0]
 
 # mainframe serves everybody: sum all user bandwith
-m.add_constraint(sum(bv.values()) <= MAX_MAINFRAME_BW,
+m.add_constraint(mainframe_bw <= MAX_MAINFRAME_BW,
                  ctname="mainframe {} bw limit".format(mainframe))
+
+print("Setup mainframe", end='\r')
 
 routers = g.get_in_edges(mainframe)[:, 0]
 for router in routers:
+    print("Router {}".format(router))
+
     # count total bandwith crossing this router
     router_bw = 0
 
     dslams = g.get_in_edges(router)[:, 0]
     for dslam in dslams:
+        print("DSLAM {}".format(dslam), end='\r')
+
         users = g.get_in_edges(dslam)[:, 0]
 
-        dslam_bw = sum(bv[v] for v in users)
-        m.add_constraint(users_bw <= MAX_DSLAM_BW,
+        dslam_bw = sum(bw[v] for v in users)
+        m.add_constraint(dslam_bw <= MAX_DSLAM_BW,
                          ctname="dslam {} bw limit".format(dslam))
 
         # mark the contribution towards the router
-        router_total_bw += dslam_bw
+        router_bw += dslam_bw
 
     m.add_constraint(router_bw <= MAX_ROUTER_BW,
                      ctname="router {} bw limit".format(router))
