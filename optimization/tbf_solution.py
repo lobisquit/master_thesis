7c87451b8ca7e82df0d3925ec3ed3ba6398ad04b
Enrico Lovisotto <enrico.lovisotto@tutanota.com>
2019-03-18 19:02:33 +0100

    Twicked heuristic algorithm, added log

--- optimization/tbf_solution.py
+++ optimization/tbf_solution.py
@@ -1,13 +1,27 @@
 import itertools
+import logging
 from random import choice, random, seed
 
+import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
 from scipy.optimize import LinearConstraint, minimize
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, lil_matrix
 
 from problem_def import *
+
+logger = logging.getLogger('aachen_net.org')
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+formatter = logging.Formatter("%(asctime)s::%(levelname)s::%(module)s::%(message)s",
+                              "%Y-%m-%d %H:%M:%S")
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 cache = {}
 def get_subtree_leaves(g, v):
@@ -36,8 +50,8 @@ def build_validator(users, g, n_leaves):
     n_constraints = n_nodes - n_leaves
 
     # constraints matrix
-    A = csr_matrix((n_constraints, n_nodes))
-    b = np.zeros( (n_constraints, 1) )
+    A = lil_matrix((n_constraints, n_nodes))
+    b = np.zeros( (n_constraints, ) )
 
     current_node_idx = 0
 
@@ -59,22 +73,29 @@ def build_validator(users, g, n_leaves):
         b[current_node_idx] = MAX_ROUTER_BW
         current_node_idx += 1
 
+    A = csr_matrix(A)
+
     def is_valid(bws):
         test = A.dot(bws) - b
-        return np.all(test < 0)
+        return np.all(test <= 0)
 
     return is_valid
 
-def perturb(bws, users, max_perturb):
-    user = choice(users)
-    delta = random() * max_perturb
+def dummy_validator(g, bws):
+    mainframe = [node for node, attrs in g.nodes(data=True) if attrs['root']][0]
 
-    # given problem specification, going forward is (almost always) a good
-    # idea: never go back
-    new_bws = bws.copy()
-    new_bws[user] += delta
+    mainframe_users = get_subtree_leaves(g, mainframe)
+    assert bws[mainframe_users].sum() < MAX_MAINFRAME_BW
 
-    return new_bws
+    for router in get_children(g, mainframe):
+        router_users = get_subtree_leaves(g, router)
+        assert bws[router_users].sum() < MAX_ROUTER_BW
+
+        for dslam in get_children(g, router):
+            dslam_users = get_subtree_leaves(g, dslam)
+            assert bws[dslam_users].sum() < MAX_DSLAM_BW
+
+    return True
 
 p_nothing = 0.2
 p_streaming = 0.6
@@ -106,29 +127,44 @@ for leaf in leaves:
         tolerances[leaf] = tolerance
         margins[leaf] = margin
 
+streaming_users = np.where(bws_min == bws_min.max())[0]
+
 users = np.array(users)
+n_users = len(users)
 is_valid = build_validator(users, g, len(leaves))
 
 # initial guess
 bws = np.zeros( (n_nodes,) )
-bws[users] = 1
+bws[users] = 500e3
+
+assert is_valid(bws), "Initial solution not valid"
 
 # start with a nice perturbation
-temperature = 1e5 # bit/s
+temperature = 500e3 # bit/s
+size = 10
 
-TEMP_DROP = 0.9
-TEMP_STEP = 1000
-MAX_BLOCKED_ITERS = 10000
+SIZE_DROP = 0.9
+TEMP_DROP = 0.99
+EPOCH = 10e3
+MAX_BLOCKED_ITERS = n_users
 
 n_iter = 1
 n_blocked_iters = 1
 while True:
-    new_bws = perturb(bws, users, temperature)
+    user = np.random.choice(streaming_users,
+                            size=(np.ceil(size).astype(int),))
+
+    delta = np.random.random(user.shape) * temperature
+
+    # given problem specification, going forward is (almost always) a good
+    # idea: never go back
+    bws[user] += delta
 
     if is_valid(bws):
-        bws = new_bws
         n_blocked_iters = 1
     else:
+        # revert change
+        bws[user] -= delta
         n_blocked_iters += 1
 
     n_iter += 1
@@ -138,9 +174,13 @@ while True:
         print("EXIT: idle for {} iterations".format(n_blocked_iters))
         break
 
-    print("Temperature", temperature, 'n_iter', n_iter)
-
-    if n_iter % TEMP_STEP == 0:
+    if n_iter % EPOCH == 0:
         temperature *= TEMP_DROP
+        size *= SIZE_DROP
 
-print("OBJ:", obj_function(bws, bws_min, tolerances, margins))
+    if n_iter % 10000 == 0:
+        obj = obj_function(bws, bws_min, tolerances, margins)
+        logger.info("OBJ: {}, T {}".format(obj, temperature))
+
+obj = obj_function(bws, bws_min, tolerances, margins)
+logger.info("RESULT: {}".format(obj))
